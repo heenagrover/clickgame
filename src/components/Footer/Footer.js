c4930d3da9f50359e0a3eca4a1d57d4747384075
Heena Grover <heenagrover1986@gmail.com>
2018-09-27 18:39:00 -0400

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer";
+
+const Footer = () => (
+	<footer className="footer">
+		<div className="footer-bottom">
+		</div>
+	</footer>
+
+);
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => (
+   <header className="header">
+   	<h2>Click on an image but don't click on any more than once!</h2>
+   </header>
+);
+export default Header;
