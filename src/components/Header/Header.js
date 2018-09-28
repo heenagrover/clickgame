fe32d70f75e5b19bc00afaff93ec9e7473d65864
Heena Grover <heenagrover1986@gmail.com>
2018-09-27 20:47:01 -0400

    Updates

--- src/components/Header/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Header.css";
-
-const Header = () => (
-   <header className="header">
-   	<h2>Click on an image and remember not to click on it more than once!</h2>
-   </header>
-);
-export default Header;
